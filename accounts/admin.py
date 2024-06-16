120b86194fcec9e37ca80e361e2830458da6557d
Nicolas Burbano <nicolas.rnbp@gmail.com>
2024-06-16 07:11:52 -0500

    add customer to accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Customer
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ("username", "email", "is_staff")
+    search_fields = ("username", "email")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("email",)}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
-    pass
+    first_name = None
+    last_name = None
+
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = AbstractUser.first_name
+    last_name = AbstractUser.last_name
+    document_number = models.CharField(max_length=20)
+    phone = PhoneNumberField()
