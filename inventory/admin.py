6f4731facba73b29aba705a008fa74358f5b45bd
Nicolas Burbano <nicolas.rnbp@gmail.com>
2024-06-18 08:12:32 -0500

    fix orders

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from inventory.models import Product
+from inventory.models import Product, Order, OrderDetail
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(OrderDetail)
+class OrderDetailAdmin(admin.ModelAdmin):
     pass
